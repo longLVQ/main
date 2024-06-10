024341da9ed1bac612a90c7e32bc455e0833fa35
long.levanquoc@nashtechglobal.com <long.levanquoc@nashtechglobal.com>
2024-06-10 10:41:13 +0000

    API definition transferred by SwaggerHub

--- swagger/src/main/java/com/evri/swagger/apis/SwaggerHubApi.java
+++ swagger/src/main/java/com/evri/swagger/apis/SwaggerHubApi.java
@@ -39,7 +39,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-@jakarta.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2024-06-10T10:40:08.579300243Z[GMT]")
+@jakarta.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2024-06-10T10:41:13.353196269Z[GMT]")
 @Validated
 public interface SwaggerHubApi {
 
--- swagger/src/main/java/com/evri/swagger/dto/SwaggerDto.java
+++ swagger/src/main/java/com/evri/swagger/dto/SwaggerDto.java
@@ -14,7 +14,7 @@ import jakarta.validation.constraints.*;
  * SwaggerDto
  */
 @Validated
-@jakarta.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2024-06-10T10:40:08.579300243Z[GMT]")
+@jakarta.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2024-06-10T10:41:13.353196269Z[GMT]")
 
 
 public class SwaggerDto   {
