27ade900fc09aae672c015499b05ddc3a81f613b
long.levanquoc@nashtechglobal.com <long.levanquoc@nashtechglobal.com>
2024-06-10 11:02:51 +0000

    API definition transferred by SwaggerHub

--- swagger/src/main/java/com/evri/swagger/apis/SwaggerHubApi.java
+++ swagger/src/main/java/com/evri/swagger/apis/SwaggerHubApi.java
@@ -39,7 +39,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-@jakarta.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2024-06-10T10:56:09.386668424Z[GMT]")
+@jakarta.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2024-06-10T11:02:51.697529895Z[GMT]")
 @Validated
 public interface SwaggerHubApi {
 
