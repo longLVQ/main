7adb1f852982c64bea97c18ef648e2235f4011f9
long.levanquoc@nashtechglobal.com <long.levanquoc@nashtechglobal.com>
2024-06-10 10:56:09 +0000

    API definition transferred by SwaggerHub

--- swagger/src/main/java/com/evri/swagger/dto/SwaggerDto.java
+++ swagger/src/main/java/com/evri/swagger/dto/SwaggerDto.java
@@ -14,7 +14,7 @@ import jakarta.validation.constraints.*;
  * SwaggerDto
  */
 @Validated
-@jakarta.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2024-06-10T10:55:31.110367619Z[GMT]")
+@jakarta.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2024-06-10T10:56:09.386668424Z[GMT]")
 
 
 public class SwaggerDto   {
