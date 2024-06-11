3e444b1a9e760088538b9eec37b732a91beadf2e
long.levanquoc@nashtechglobal.com <long.levanquoc@nashtechglobal.com>
2024-06-11 07:00:33 +0000

    update

--- src/main/java/com/evri/swagger/dto/SwaggerDtoTest.java
+++ src/main/java/com/evri/swagger/dto/SwaggerDtoTest.java
@@ -14,7 +14,7 @@ import jakarta.validation.constraints.*;
  * SwaggerDtoTest
  */
 @Validated
-@jakarta.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2024-06-11T06:54:44.463738839Z[GMT]")
+@jakarta.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2024-06-11T07:00:33.107080774Z[GMT]")
 
 
 public class SwaggerDtoTest   {
